72967134ca483efc02d6ad14927f0a39f8a7f570
alexjendra <khushbue51@gmail.com>
2024-02-01 21:28:51 +0000

    my backend work

---
+++ src/test/java/com/virgingames/cucumber/CucumberRunner.java
@@ -0,0 +1,11 @@
+package com.virgingames.cucumber;
+
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features = "src/test/resources/feature" )
+public class CucumberRunner {
+}
---
+++ src/test/java/com/virgingames/steps/MyStepdefs.java
@@ -0,0 +1,15 @@
+package com.virgingames.steps;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MyStepdefs {
+    @When("User sends a GET request to list endpoint")
+    public void userSendsAGETRequestToListEndpoint() {
+
+    }
+
+    @Then("User must get back a valid status code {int}")
+    public void userMustGetBackAValidStatusCode(int arg0) {
+    }
+}
